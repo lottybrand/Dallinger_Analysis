42951558323fb801ef34767c389e679812bf2062
Charlotte Brand <git>
2020-03-27 17:21:23 +0000

    Create dropouts.R
    thinking about dropout data

---
+++ dropouts.R
@@ -0,0 +1,12 @@
+#Looking out dropout data for reviewer
+
+finished <-finalScore$u_origin
+question1 <- full_data[full_data$number==1,]
+View(question1)
+started <-question1$u_origin
+dropOuts <- started %in% finished
+summary(dropOuts)
+
+# check if dropouts have higher or lower copying behaviour than average?
+# check if have higher/lower score than average? 
+# anything else?? 
